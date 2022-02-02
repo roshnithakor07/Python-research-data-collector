1ed5572950138b3ff84179d26b1ee99ff01e20f4
Roshni Thakor <74705502+roshnithakor07@users.noreply.github.com>
2022-02-02 22:04:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from bs4 import BeautifulSoup
 
 def query():
     
-    user_query = input('Enter your query: ')
+    user_query = input('Enter Your Query: ')
 
-    URL = "https://www.google.co.in/search?q=" + user_query + "linkedin"
+    URL = "https://www.google.co.in/search?q=" + user_query 
     headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
     }
@@ -14,11 +14,11 @@ def query():
     page = requests.get(URL, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    # to Get  Owner Name
+    # To Get  Owner Name
     Owner_name = soup.find(class_ ='LC20lb MBeuO DKV0Md').get_text()
     print("Owner Name:" ,Owner_name)
 
-    #for Linkedin Profile link
+    #To Get Linkedin Profile link
     answer = soup.find(class_="yuRUbf")
     linkedin_Profile_link = answer.find('a').get('href')
     print("Linkedin Profile link:",linkedin_Profile_link)
@@ -31,7 +31,7 @@ while True:
     try:
         query()
     except Exception:
-        print('Sorry no result, please be clear')
+        print('Sorry no result')
     user_input = input('To continue press y: ')
     if user_input != 'y':
         break
